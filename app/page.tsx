57d97cc48d2d4c86664f749d564ea5777d32e230
Savestate1 <davidisaaclacson69@gmail.com>
2023-11-16 16:03:19 +0800

    Add blank

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,7 @@
 import Login from "./login/page";
 
-
-<<<<<<< HEAD
-import Navbar from './components/navbar/navbar'
-import PostCard from './components/navbar/card/card'
-import FilterBar from './components/navbar/filter/filter'
-
-export default function Home() {
-  return (
-
-    <div>
-      <div>
-        <Navbar />
-      </div>
-
-      <div>
-      <FilterBar/>
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <blank/>
-    </div>
-
-
-    </div>
-    
-=======
-
 export default function Home() {
   return (
       <Login />
->>>>>>> 1469f955862bcdad2c6575b26928e96eb803d029
   )
 }
